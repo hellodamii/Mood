be1a243833b2c88fe8a82a82308c796e77747623
hellodamii <daremiarchs@gmail.com>
2025-06-04 17:14:47 +0100

    Initial Commit

---
+++ Mood/MoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodApp.swift
+//  Mood
+//
+//  Created by Busha on 04/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

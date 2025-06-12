4a445d8ae2885e1fdff2e1c9eac4b4f4e71ba8f7
hellodamii <daremiarchs@gmail.com>
2025-06-12 16:42:07 +0100

    historyview

---
+++ Mood/Views/HistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryView.swift
+//  Mood
+//
+//  Created by Busha on 12/06/2025.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryView()
+}
